0acad79f33e4271fad4577f67c4042f060ea5e45
Angel-TheDev <angelantoinetted@yahoo.com>
2019-05-14 17:22:59 -0400

    first commit

---
+++ Fortune Teller Correction/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Fortune_Teller_Correction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your first name?");
+            string name = Console.ReadLine();
+            Console.WriteLine(""); 
+            Console.WriteLine("What is your last name?");
+            Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("{1<100}");
+          
+
+            Console.WriteLine("What is your favorite ROYGBIV color? Enter 'Help' for list of ROYGBIV colors.");
+            Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("How many siblings do you have?");
+            Console.ReadLine();
+            Console.WriteLine("");
+
+
+           
+            
+            
+        
+
+
+           
+        
+
+        }
+    }
+}
